080534ef48bffff251eb2d957631c9045b6fde9d
soizhiwen <zhiwen39@gmail.com>
2024-12-09 11:30:49 +0100

    Add CRUD and routes

--- api/routers/plans.py
+++ api/routers/plans.py
@@ -1,7 +1,9 @@
 import uuid
+from datetime import datetime
 
-from fastapi import APIRouter, BackgroundTasks, HTTPException
+from fastapi import APIRouter, HTTPException
 
+from api import logger
 from api.chains.plans import create_plan_chain
 from api.schemas import CreatePlanInput, CreatePlanOutput, ReadPlanOutput
 from api.database import plans
@@ -10,18 +12,26 @@ router = APIRouter(prefix="/plans")
 
 
 @router.post("/", tags=["plans"])
-async def create_plan(
-    plan: CreatePlanInput, background_tasks: BackgroundTasks
-) -> CreatePlanOutput:
-    id = uuid.uuid4()
-    background_tasks.add_task(create_plan_chain, id, plan.budget, plan.total_days)
-    return CreatePlanOutput(id=id)
+async def create_plan(plan: CreatePlanInput) -> CreatePlanOutput:
+    try:
+        id = uuid.uuid4()
+        data = await create_plan_chain(id, **plan.model_dump())
+        data["id"] = id
+        data["budget"] = plan.budget
+        data["total_days"] = plan.total_days
+        data["timestamp"] = datetime.now()
+        plans.create(data)
+        return CreatePlanOutput(id=id)
+    except Exception as e:
+        logger.error(f"Failed to create plan: {e}")
+        raise HTTPException(status_code=500, detail="Failed to create plan.")
 
 
 @router.get("/{id}", tags=["plans"])
 async def read_plan(id: uuid.UUID) -> ReadPlanOutput:
-    plan = plans.read(id)
-    if plan:
-        return ReadPlanOutput.model_validate(plan)
-
-    raise HTTPException(status_code=404, detail="Plan not found")
+    try:
+        data = plans.read(id)
+        return ReadPlanOutput.model_validate(data)
+    except Exception as e:
+        logger.error(f"Failed to read plan: {e}")
+        raise HTTPException(status_code=404, detail="Plan not found.")
