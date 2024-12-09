e88261d7d363854ff68760a52aacc54feab376ff
soizhiwen <zhiwen39@gmail.com>
2024-12-09 22:59:44 +0100

    Refactor second version

--- api/routers/plans.py
+++ api/routers/plans.py
@@ -16,7 +16,7 @@ from api.schemas import (
 router = APIRouter(prefix="/plans")
 
 
-@router.post("/", tags=["plans"])
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=CreatePlanOutput)
 async def create_plan(data: CreatePlanInput, req: Request) -> CreatePlanOutput:
     try:
         data = data.model_dump()
@@ -28,10 +28,13 @@ async def create_plan(data: CreatePlanInput, req: Request) -> CreatePlanOutput:
         return CreatePlanOutput(id=new_plan.inserted_id)
     except Exception as e:
         logger.error(f"Failed to create plan: {e}")
-        raise HTTPException(status_code=500, detail="Failed to create plan.")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Failed to create plan.",
+        )
 
 
-@router.get("/", tags=["plans"])
+@router.get("/", response_model=list[ReadPlansOutput])
 async def read_plans(req: Request, limit: int = 10) -> list[ReadPlansOutput]:
     try:
         plans = req.app.db["plans"].find(
@@ -43,26 +46,35 @@ async def read_plans(req: Request, limit: int = 10) -> list[ReadPlansOutput]:
         return list(plans)
     except Exception as e:
         logger.error(f"Failed to read plans: {e}")
-        raise HTTPException(status_code=404, detail="Plans not found.")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Plans not found."
+        )
 
 
-@router.get("/{id}", tags=["plans"])
+@router.get("/{id}", response_model=ReadPlanOutput)
 async def read_plan(id: PyObjectId, req: Request) -> ReadPlanOutput:
     try:
         plan = req.app.db["plans"].find_one({"_id": id})
-        return plan
+        if plan is not None:
+            return plan
+        raise Exception("Plan not found.")
     except Exception as e:
         logger.error(f"Failed to read plan: {e}")
-        raise HTTPException(status_code=404, detail="Plan not found.")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Plan not found."
+        )
 
 
-@router.delete("/{id}", tags=["plans"])
+@router.delete("/{id}")
 async def delete_plan(id: PyObjectId, req: Request, res: Response) -> Response:
     try:
         deleted_plan = req.app.db["plans"].delete_one({"_id": id})
         if deleted_plan.deleted_count == 1:
             res.status_code = status.HTTP_204_NO_CONTENT
             return res
+        raise Exception("Plan not found.")
     except Exception as e:
         logger.error(f"Failed to delete plan: {e}")
-        raise HTTPException(status_code=404, detail="Plan not found.")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Plan not found."
+        )
