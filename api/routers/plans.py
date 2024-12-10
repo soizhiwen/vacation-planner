51d38a07b8dcc5934f9496894a090eeed353339e
soizhiwen <zhiwen39@gmail.com>
2024-12-10 09:39:05 +0100

    Remove try catch

--- api/routers/plans.py
+++ api/routers/plans.py
@@ -18,63 +18,38 @@ router = APIRouter(prefix="/plans")
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=CreatePlanOutput)
 async def create_plan(data: CreatePlanInput, req: Request) -> CreatePlanOutput:
-    try:
-        data = data.model_dump()
-        plan = await create_plan_chain(**data)
-        plan = plan.model_dump()
-        plan["timestamp"] = datetime.now()
-        plan = data | plan
-        new_plan = req.app.db["plans"].insert_one(plan)
-        return CreatePlanOutput(id=new_plan.inserted_id)
-    except Exception as e:
-        logger.error(f"Failed to create plan: {e}")
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Failed to create plan.",
-        )
+    data = data.model_dump()
+    plan = await create_plan_chain(**data)
+    plan = plan.model_dump()
+    plan["timestamp"] = datetime.now()
+    plan = data | plan
+    new_plan = req.app.db["plans"].insert_one(plan)
+    return {"id": new_plan.inserted_id}
 
 
 @router.get("/", response_model=list[ReadPlansOutput])
 async def read_plans(req: Request, limit: int = 10) -> list[ReadPlansOutput]:
-    try:
-        plans = req.app.db["plans"].find(
-            {},
-            {"_id": 1, "title": 1, "description": 1},
-            sort=[("timestamp", pymongo.DESCENDING)],
-            limit=limit,
-        )
-        return list(plans)
-    except Exception as e:
-        logger.error(f"Failed to read plans: {e}")
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Plans not found."
-        )
+    plans = req.app.db["plans"].find(
+        {},
+        {"_id": 1, "title": 1, "description": 1},
+        sort=[("timestamp", pymongo.DESCENDING)],
+        limit=limit,
+    )
+    return list(plans)
 
 
 @router.get("/{id}", response_model=ReadPlanOutput)
 async def read_plan(id: PyObjectId, req: Request) -> ReadPlanOutput:
-    try:
-        plan = req.app.db["plans"].find_one({"_id": id})
-        if plan is not None:
-            return plan
-        raise Exception("Plan not found.")
-    except Exception as e:
-        logger.error(f"Failed to read plan: {e}")
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Plan not found."
-        )
+    plan = req.app.db["plans"].find_one({"_id": id})
+    if plan is not None:
+        return plan
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Plan not found.")
 
 
 @router.delete("/{id}")
 async def delete_plan(id: PyObjectId, req: Request, res: Response) -> Response:
-    try:
-        deleted_plan = req.app.db["plans"].delete_one({"_id": id})
-        if deleted_plan.deleted_count == 1:
-            res.status_code = status.HTTP_204_NO_CONTENT
-            return res
-        raise Exception("Plan not found.")
-    except Exception as e:
-        logger.error(f"Failed to delete plan: {e}")
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Plan not found."
-        )
+    deleted_plan = req.app.db["plans"].delete_one({"_id": id})
+    if deleted_plan.deleted_count == 1:
+        res.status_code = status.HTTP_204_NO_CONTENT
+        return res
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Plan not found.")
