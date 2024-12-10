99e65c6c456c418928d29b0b2dd4f8e163179a46
soizhiwen <zhiwen39@gmail.com>
2024-12-10 10:19:20 +0100

    Override exception

--- api/main.py
+++ api/main.py
@@ -2,9 +2,15 @@ import os
 from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
 from pymongo import MongoClient
+from fastapi import FastAPI, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import HTTPException, RequestValidationError
+from starlette.requests import Request
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
+from api import logger
 from api.routers import plans
 
 
@@ -23,3 +29,18 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(plans.router)
+
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request: Request, exc: HTTPException) -> JSONResponse:
+    logger.error(exc)
+    return JSONResponse({"detail": exc.detail}, status_code=exc.status_code)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(
+    request: Request, exc: RequestValidationError
+) -> JSONResponse:
+    logger.error(exc)
+    exc = jsonable_encoder(exc.errors())
+    return JSONResponse(exc, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
--- api/routers/plans.py
+++ api/routers/plans.py
@@ -18,38 +18,46 @@ router = APIRouter(prefix="/plans")
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=CreatePlanOutput)
 async def create_plan(data: CreatePlanInput, req: Request) -> CreatePlanOutput:
+    logger.info(f"Creating plan: {data}")
     data = data.model_dump()
     plan = await create_plan_chain(**data)
     plan = plan.model_dump()
     plan["timestamp"] = datetime.now()
     plan = data | plan
     new_plan = req.app.db["plans"].insert_one(plan)
+    logger.info(f"Plan created: {new_plan.inserted_id}")
     return {"id": new_plan.inserted_id}
 
 
 @router.get("/", response_model=list[ReadPlansOutput])
 async def read_plans(req: Request, limit: int = 10) -> list[ReadPlansOutput]:
+    logger.info(f"Reading plans: {limit}")
     plans = req.app.db["plans"].find(
         {},
         {"_id": 1, "title": 1, "description": 1},
         sort=[("timestamp", pymongo.DESCENDING)],
         limit=limit,
     )
+    logger.info(f"Plans read: {plans}")
     return list(plans)
 
 
 @router.get("/{id}", response_model=ReadPlanOutput)
 async def read_plan(id: PyObjectId, req: Request) -> ReadPlanOutput:
+    logger.info(f"Reading plan: {id}")
     plan = req.app.db["plans"].find_one({"_id": id})
     if plan is not None:
+        logger.info(f"Plan read: {plan}")
         return plan
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Plan not found.")
 
 
 @router.delete("/{id}")
 async def delete_plan(id: PyObjectId, req: Request, res: Response) -> Response:
+    logger.info(f"Deleting plan: {id}")
     deleted_plan = req.app.db["plans"].delete_one({"_id": id})
     if deleted_plan.deleted_count == 1:
         res.status_code = status.HTTP_204_NO_CONTENT
+        logger.info(f"Plan deleted: {id}")
         return res
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Plan not found.")
