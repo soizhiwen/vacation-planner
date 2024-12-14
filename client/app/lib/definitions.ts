10f035e6a7e026470a87c2bc3f1fec58d0a4b0e3
soizhiwen <zhiwen39@gmail.com>
2024-12-14 21:33:06 +0100

    Define types

---
+++ client/app/lib/definitions.ts
@@ -0,0 +1,21 @@
+export type Activity = {
+    time: string;
+    place: string;
+    activity: string;
+    notes: string;
+}
+
+export type Day = {
+    day: number;
+    activities: Activity[];
+}
+
+export type Plan = {
+    _id: string;
+    title: string;
+    description: string;
+    budget: number;
+    totalDays: number;
+    days: Day[];
+    timestamp: Date;
+}
