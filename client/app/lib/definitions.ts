2acd6926e676b683a69759dc875981446a0c3b3f
soizhiwen <zhiwen39@gmail.com>
2024-12-15 22:10:14 +0100

    Add state definition

--- client/app/lib/definitions.ts
+++ client/app/lib/definitions.ts
@@ -1,14 +1,22 @@
+export type CreatePlanState = {
+    errors?: {
+        budget?: string[];
+        totalDays?: string[];
+    };
+    message?: string | null;
+};
+
 export type Activity = {
     time: string;
     place: string;
     activity: string;
     notes: string;
-}
+};
 
 export type Day = {
     day: number;
     activities: Activity[];
-}
+};
 
 export type Plan = {
     _id: string;
@@ -18,12 +26,12 @@ export type Plan = {
     totalDays: number;
     days: Day[];
     timestamp: string;
-}
+};
 
 export type PlanWithHeader = {
     _id: string;
     title: string;
     description: string;
-}
+};
 
 export type Plans = Plan[];
